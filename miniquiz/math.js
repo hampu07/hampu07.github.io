f0a216f02f2ea22c2c8eb0bea6352d90804fa553
NoelNim <noel@nimstad.com>
2021-03-25 17:41:17 +0100

    yes

--- miniquiz/math.js
+++ miniquiz/math.js
@@ -1,25 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => 
 {
-    console.log('MILK');
-
-    let gayColors = 
-    [
-        '#fa5c5c',
-        '#faa65c',
-        '#fae85c',
-        '#74fa5c',
-        '#5cd2fa',
-        '#5f5cfa',
-        '#ae5cfa',
-        '#fa5ced'
-    ]
-
-    let randomGayColor = gayColors[Math.floor(Math.random() * gayColors.length)]
-
-    document.body.style.background = randomGayColor;
+    console.log('jag tog bort alla fusk //NoelNim');
 
     let a = Math.floor(Math.random() * 101);
-    let b = Math.floor(Math.random() * 11);
+    let b = Math.floor(Math.random() * 19) + 2;
     let c = (a / b);
 
     function checkforrules()
@@ -27,31 +11,54 @@ document.addEventListener('DOMContentLoaded', () =>
         if(b > a)
         {
             a = Math.floor(Math.random() * 101);
-            b = Math.floor(Math.random() * 11);
+            b = Math.floor(Math.random() * 19) + 2;
             c = (a / b)
             checkforrules();
         } else if(c % 1 != 0)
         {
             a = Math.floor(Math.random() * 101);
-            b = Math.floor(Math.random() * 11);
+            b = Math.floor(Math.random() * 19) + 2;
             c = (a / b)
             checkforrules();
         } else return
     }
 
-    checkforrules()
-    console.log(a, b, c)
+    checkforrules();
 
-    const gone1 = document.getElementById('gone');
+    const gone1 = document.getElementById('gone1');
     const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    const awnser = document.getElementById('awsome');
+    const resultat = document.getElementById('resultat');
     const text1 = document.getElementById('text1');
-    const text2 = document.getElementById('text2');
 
-    btn1.addEventListener('click', start1)
+    btn1.addEventListener('click', start1);
 
     function start1()
     {
-        gone1.innerHTML = " ";
-        text1.innerHTML = a + "<br/>" + b + "<br/>" + "______"
+        gone1.innerHTML = "";
+        text1.innerHTML = a + " <span>&#247</span> " + b + " =";
+    }
+
+    btn2.addEventListener('click', awnserYes);
+
+    function awnserYes()
+    {
+        if(gone1.innerHTML != "")
+        {
+            text1.innerHTML = '<span>du m&#229ste starta quizet f&oumlre du kan svara</span>';
+        } else if(awnser.value == c)
+        {
+            resultat.innerHTML = 'resultat: <i class="fas fa-check-circle fa-lg"></i>';
+            a = Math.floor(Math.random() * 101);
+            b = Math.floor(Math.random() * 19) + 2;
+            c = (a / b);
+            checkforrules();
+            text1.innerHTML = a + " / " + b + " =";
+            awnser.value = "";
+        } else
+        {
+            resultat.innerHTML = 'resultat: <i class="fas fa-times fa-lg"></i>';
+        }
     }
 })
